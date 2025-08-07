faf63379470e04c477f6a56041e38ff9152469e0
dzj <2091088157.qq.com>
2025-08-07 19:37:22 +0800

    try

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,12 +32,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
-        if let std::collections::hash_map::Entry::Vacant(e)=basket.entry(fruit)
-        {
-            e.insert(3);
-        }else {
-           
-        }
+        basket.entry(fruit).or_insert(2);
     }
 }
 
